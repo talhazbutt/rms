6df6871e45c6d368ee61f888e6b630cddadc292f
Talha Zubair Butt <talhazubairbutt@gmail.com>
2018-12-04 22:18:12 +0500

    Add files via upload

--- app/src/main/java/com/example/lenovo/eats/ClassModel/Order.java
+++ app/src/main/java/com/example/lenovo/eats/ClassModel/Order.java
@@ -7,30 +7,50 @@ import java.util.HashMap;
  */
 
 public class Order {
-    HashMap<String,Ingredient> menuItems;
-    String specialInstruction;
+    HashMap<String,Integer> order_items;
+    String special_instruction;
+    Long time_of_completion;
+    Long timestamp;
 
-    public Order(String specialInstruction) {
-        this.specialInstruction = specialInstruction;
-        menuItems=new HashMap<>();
+    public Order(HashMap<String, Integer> order_items, String special_instruction, Long time_of_completion, Long timestamp) {
+        this.order_items = order_items;
+        this.special_instruction = special_instruction;
+        this.time_of_completion = time_of_completion;
+        this.timestamp = timestamp;
+    }
+
+    public HashMap<String, Integer> getOrder_items() {
+        return order_items;
+    }
+
+    public void setOrder_items(HashMap<String, Integer> order_items) {
+        this.order_items = order_items;
+    }
+
+    public String getSpecial_instruction() {
+        return special_instruction;
+    }
+
+    public void setSpecial_instruction(String special_instruction) {
+        this.special_instruction = special_instruction;
+    }
+
+    public Long getTime_of_completion() {
+        return time_of_completion;
+    }
+
+    public void setTime_of_completion(Long time_of_completion) {
+        this.time_of_completion = time_of_completion;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
     }
 
     public Order() {
-    }
-
-    public HashMap<String, Ingredient> getMenuItems() {
-        return menuItems;
-    }
-
-    public void setMenuItems(HashMap<String, Ingredient> menuItems) {
-        this.menuItems = menuItems;
-    }
-
-    public String getSpecialInstruction() {
-        return specialInstruction;
-    }
-
-    public void setSpecialInstruction(String specialInstruction) {
-        this.specialInstruction = specialInstruction;
     }
 }
